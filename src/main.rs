f000d9814d1b4fc277392d5336c4313de6e21c5f
eniolajayi <ajayieniola16@gmail.com>
2023-02-12 02:28:18 +0100

    write run file function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::env;
+use std::fs::File;
+use std::io;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+        panic!("Usage: kuskit [script]");
+    } else if args.len() == 2 {
+    } else {
+        run_prompt();
+    }
+
+    println!("{:?}", &args);
+}
+
+fn run_file(filename: impl AsRef<Path>) {
+    let file = match File::open(filename) {
+        Ok(file) => file,
+        Err(error) => {
+            println!("An error occurred while opening the file: {:?}", error);
+            return;
+        },
+    };
+
+    let reader = io::BufReader::new(file);
+    match io::read_to_string(reader) {
+        Ok(s) => run(s),
+        Err(e) => eprintln!("{e}"),
+    }
+}
+
+fn run(source: String) {
+    todo!();
+}
+
+fn run_prompt() {
+    todo!();
 }
