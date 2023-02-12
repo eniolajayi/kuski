d011e288dcf306dff9c1abbc6893b9b3e60f572c
eniolajayi <ajayieniola16@gmail.com>
2023-02-12 03:06:42 +0100

    create repl

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::fs::File;
-use std::io;
+use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
@@ -23,20 +23,30 @@ fn run_file(filename: impl AsRef<Path>) {
         Err(error) => {
             println!("An error occurred while opening the file: {:?}", error);
             return;
-        },
+        }
     };
 
     let reader = io::BufReader::new(file);
     match io::read_to_string(reader) {
-        Ok(s) => run(s),
+        Ok(s) => run(&s),
         Err(e) => eprintln!("{e}"),
     }
 }
 
-fn run(source: String) {
+fn run(source: &String) {
     todo!();
 }
 
 fn run_prompt() {
-    todo!();
+    println!("kuski repl >> q/quit to exit");
+    loop {
+        print!("> ");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        if input.trim() == "quit" || input.trim() == "q" {
+            break;
+        }
+        run(&input);
+    }
+    println!(">> ");
 }
