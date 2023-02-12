b6fea3b441bb9d6487be218e97c5556942f16483
eniolajayi <ajayieniola16@gmail.com>
2023-02-12 02:29:37 +0100

    pass filename to run_file function

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() {
     if args.len() > 2 {
         panic!("Usage: kuskit [script]");
     } else if args.len() == 2 {
+        run_file(&args[1]);
     } else {
         run_prompt();
     }
