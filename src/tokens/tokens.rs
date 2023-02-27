27dbdbec02c5a0ab5f0258e9287e7fcef4f32d10
eniolajayi <ajayieniola16@gmail.com>
2023-02-27 16:53:05 +0100

    add create function for Token struct

--- src/tokens/tokens.rs
+++ src/tokens/tokens.rs
@@ -1,25 +1,67 @@
 pub enum TokenType {
     // Single-character tokens.
-  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
 
-  // One or two character tokens.
-  BANG, BANG_EQUAL,
-  EQUAL, EQUAL_EQUAL,
-  GREATER, GREATER_EQUAL,
-  LESS, LESS_EQUAL,
+    // One or two character tokens.
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
 
-  // Literals.
-  IDENTIFIER, STRING, NUMBER,
+    // Literals.
+    IDENTIFIER,
+    STRING,
+    NUMBER,
 
-  // Keywords.
-  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
-  PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+    // Keywords.
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
 
-  EOF
+    EOF,
 }
 
 pub struct Token {
-    token_type: TokenType,
+    tkn_type: TokenType,
     lexeme: String,
+    literal: T,
+    line: u32,
+}
+
+impl Token {
+    pub fn create(tkn_type: TokenType, lexeme: String, literal: T, line: u32) -> Token {
+        Token {
+            tkn_type,
+            lexeme,
+            literal,
+            line,
+        }
+    }
 }
