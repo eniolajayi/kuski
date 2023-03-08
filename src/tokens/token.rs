bc47147c0e9d195c93443d3d613495000458c6a8
eniolajayi <ajayieniola16@gmail.com>
2023-03-08 23:59:29 +0100

    remove field "literal"

--- src/tokens/token.rs
+++ src/tokens/token.rs
@@ -1,17 +1,17 @@
+use crate::tokens::token_type::TokenType;
+// TODO Learn more about Generic parameters in structs
 #[derive(Debug)]
 pub struct Token {
     tkn_type: TokenType,
     lexeme: String,
-    literal: T,
     line: u32,
 }
 
 impl Token {
-    pub fn create(tkn_type: TokenType, lexeme: String, literal: T, line: u32) -> Token {
+    pub fn create(tkn_type: TokenType, lexeme: String, line: u32) -> Token {
         Token {
             tkn_type,
             lexeme,
-            literal,
             line,
         }
     }
